ae34a238af7ae6ef08a975d3b7062a1357ef3b78
Olivér Kiss <Kiss_Oliver@phd.ceu.edu>
2021-10-27 10:50:19 +0200

    update fetched libffmpeg version

--- fix_opera.sh
+++ fix_opera.sh
@@ -1,7 +1,6 @@
-wget https://github.com/iteufel/nwjs-ffmpeg-prebuilt/releases/download/0.45.1/0.45.1-linux-x64.zip
-unzip 0.45.1-linux-x64.zip
+wget https://github.com/iteufel/nwjs-ffmpeg-prebuilt/releases/download/0.55.0/0.55.0-linux-x64.zip
+unzip 0.55.0-linux-x64.zip
 sudo cp libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/
 rm libffmpeg.so
-rm 0.45.1-linux-x64.zip
+rm 0.55.0-linux-x64.zip
 echo Done
-
